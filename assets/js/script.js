fe7a0046d47b535e3f385a16a5cdc0a85cc8d146
Luciano Berrios <lberrios.1402@gmail.com>
2024-06-07 19:31:22 -0400

    Avance Parte 1 finalizada al 100%
    Se realizaron cambios en las validaciones, debido a inconsistencias, ahora las validaciones funcionan al 100%
    Se actualizo el diseño de la pagina para permitir visualizar el cambio a modo oscuro (Se modifico el body original que ya era oscuro por uno claro)
    Se realizaron 3 funciones 2 para validar datos y radios y otra para cambiar el color de la pagina web y el color de la fuente

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,58 +2,119 @@ import {registrarPersona,obtenerPersonas} from "./promesas.js";
 
 window.addEventListener("load",() => {
     document.getElementById("btnRegistrar").addEventListener("click",registrar); // se agrega el listener al boton registrar
-    document.getElementById("btnContraste").addEventListener("click",contraste); // se agrega el listener al constraste
+    document.getElementById("btnContraste").addEventListener("click",contraste); // se agrega el boton de contraste
 
 });
 
-/*<select name="select" id="campeon">
-                    <option value="Dalas Mavericks">Dalas Mavericks</option>
-                    <option value="Boston Celtics">Boston Celtics</option>
-                    <option value="Minnesota Timberwolves">Minnesota Timberwolves</option>
-                    <option value="Indiana Pacers">Indiana Pacers</option>
-                  </select>
-                <input type="text" id="nombre" placeholder="Ingresa tu nombre" name="nombre">
-                <input type="text" id="apellido" placeholder="Ingresa tu apellido" name="apellido">
-                <input type="text" id="rut" name="rut" placeholder="Ingresa tu rut">
-                <input type="email" id="correo" name="correo" placeholder="Ingresa tu correo">
-                <input type="number" id="edad" name="edad" placeholder="Ingresa tu edad">
-    
-                <div id="radios">
-                    <p>En cuantos partidos se decidirá el campeon:</p>
-                    <input  type="radio" id="barrida" name="c_partidos" value="4-0">
-                    <label  for="4-0">La serie termina en 4 partidos</label><br>
-                    <input  type="radio" id="resistencia" name="c_partidos" value="4-1">
-                    <label  for="4-1">Mucha superioridad, se acaba en 5</label><br>
-                    <input  type="radio" id="igualado" name="c_partidos" value="4-2">
-                    <label for="4-2">Logran campeonar con tranquilidad en 6 partidos</label><br>
-                    <input  type="radio" id="igualado" name="c_partidos" value="4-3">
-                    <label for="4-3">Reñido, se decidira el campeon en 7 partidos</label><br>
-    
-    
-                </div>
-                <textarea name="motivo" id="motivo" cols="90" rows="10" placeholder="Cuentanos por que deberias ganar..."></textarea>
 
-                <input id="condiciones" type="checkbox">
-                <label for="terminos">Acepto los terminos y condiciones</label><br>*/
+const validaciones_front = (idCampo) => {
+    var campo = document.getElementById(idCampo).value;
+    if (campo === undefined || campo === null){
+        console.log("Linea 42 No hay nada");
+        return false;
+    }
+    campo = campo.trim();
+    if(campo == "" || (idCampo == "rut" && (campo.lenght > 8 || campo.lenght <7) ) || (idCampo == "edad" && (campo > 130 || campo < 0  ))){
+        //Se valida que no este vacio, o que si es rut sea menos de 8 caracteres o si es edad que este en un rango imposible
+        console.log("No Valido el campo: "+idCampo+" VALOR: "+campo);
 
+        document.getElementById("validar"+idCampo).style.display = "flex";
+        document.getElementById("validar"+idCampo).style.margin = "0px";
+        document.getElementById("validar"+idCampo).style.padding = "0px";
+        document.getElementById("validar"+idCampo).style.color = "red";
+        
+        document.getElementById(idCampo).style.border = "2px solid red";
+        document.getElementById(idCampo).placeholder = "Escribe tu "+idCampo;
+        var validado = false
+    }else{
+        console.log("Validado el campo "+idCampo+"de valor: "+campo);
+        document.getElementById("validar"+idCampo).style.border = "2px solid green";
+        document.getElementById("validar" + idCampo).style.display = "none";
+        document.getElementById(idCampo).style.border = "2px solid green";
+        document.getElementById(idCampo).placeholder = "Escribe tu "+idCampo;
+
+        var validado = true;
+    }
+    
+    return validado;
+
+}
+
+const validarRadio = (nombreCampo) => { //Debido a la complejidad que me supuso el radio hice una función unicamente para validarlo a el 
+    var radios = document.querySelectorAll(`input[name="${nombreCampo}"]`); //Rescatamos todos los radios esten o no seleccionados 
+    var radioValid = Array.from(radios).some(radio => radio.checked); //lo convertimos en array y vemos si alguno esta marcado si estan marcados es tru y si no false
+
+    if (!radioValid) {
+        console.log("No Valido el campo: " + nombreCampo);
+        document.getElementById("validar" + nombreCampo).style.display = "flex";
+        document.getElementById("validar" + nombreCampo).style.margin = "0px";
+        document.getElementById("validar" + nombreCampo).style.padding = "0px";
+        document.getElementById("validar" + nombreCampo).style.color = "red";
+
+        return false;
+    } else {
+        console.log("Validado el campo " + nombreCampo);
+        document.getElementById("validar" + nombreCampo).style.display = "none"; 
+        return true;
+    }
+}
  
-const registrar = async() => {              //NO FUNCIONA POR QUE LOS DATOS QUEDAN COMO UNDEFINED
-
-    // Hacemos un get para rescatar cada campo del formulario
-    let eCampeon = document.getElementById("campeon");
-    console.log(eCampeon);
-    let eNombre = document.getElementById("nombre");
-    console.log(eNombre);
-    let eApellido = document.getElementById("apellido");
-    let eRut = document.getElementById("rut");
-    let eCorreo = document.getElementById("correo");
-    let eEdad = document.getElementById("edad");
-    let eMotivo = document.getElementById("motivo");
-    let eCondiciones = document.getElementById("condiciones");
-    let eC_partidos = document.querySelector('input[name="c_partidos"]:checked')
-
-    console.log("Linea 48:"+eCondiciones+"\nLinea 50:"+eCondiciones.value);
+const registrar = async() => {       
+    /* VALIDACIONES
+     Luego de intentar validar creando el campo validacion  que sea booleano opte por crear una variable errores ya que al ser booleano si el
+     ultimo campo se validaba registraba el codigo vacio, con las siguiente implementación se logro validar todos los campos gracias a la funcion
+     validaciones_front y para el caso particular del radio 
+    */
     
+     //Se valida que no este vacio,undefined o null mandando como parametro el nombre del campo. 
+    let errores = 0;
+    // Hacemos un get para rescatar cada campo del formulario
+    let eCampeon = document.getElementById("campeon");   
+    if (!validaciones_front("campeon")){
+        errores++;
+    }; 
+
+    let eNombre = document.getElementById("nombre");
+    if (!validaciones_front("nombre")){
+        errores++;
+    }; 
+
+    let eApellido = document.getElementById("apellido");
+    if (!validaciones_front("apellido")){
+        errores++;
+    }; 
+
+    let eRut = document.getElementById("rut");
+    if (!validaciones_front("rut")){
+        errores++;
+    }; 
+
+    let eCorreo = document.getElementById("correo");
+    if (!validaciones_front("correo")){
+        errores++;
+    }; 
+
+    let eEdad = document.getElementById("edad");
+    if (!validaciones_front("edad")){
+        errores++;
+    }; 
+    
+    let eC_partidos = document.querySelector('input[name="c_partidos"]:checked')
+    
+
+    if (!validarRadio('c_partidos')){
+        errores++;
+    };
+
+    let eMotivo = document.getElementById("motivo");
+    if (!validaciones_front("motivo")){
+        errores++;
+    }; 
+    console.log(errores);
+    if (errores != 0) {
+        alert("Hay errores en el formulario ");
+        return false;
+    }
     
 
     // Ahora nuevas variables para guardar los valores de cada campo
@@ -65,32 +126,9 @@ const registrar = async() => {              //NO FUNCIONA POR QUE LOS DATOS QUED
     let vEdad = eEdad.value;
     let vC_partidos = eC_partidos.value;
     let vMotivo = eMotivo.value;
-    let vCondiciones = eCondiciones.value;
-
-    console.log("Valores"+vCampeon+vNombre+vApellido+vCorreo+vRut+vC_partidos+vMotivo+vCondiciones); //Para verificar que los valores estan guardados
-    // Validaciones
-    let Validado = true;
-    validado = validaciones_front("campeon");
-    validado = validaciones_front("nombre");
-    validado = validaciones_front("apellido");
-    validado = validaciones_front("rut");
-    validado = validaciones_front("correo");
-    validado = validaciones_front("edad");
-    validado = validaciones_front("motivo");
-
-    if (validado == false) {
-        alert("Hay errores en el formulario");
-        return false;
-    }
-
-
-
-
-
-
 
     // Creamos un objeto con los valores de cada campo
-    let objeto={campeon:vCampeon,nombre:vNombre,apellido:vApellido,correo:vCorreo,rut:vRut,edad:vEdad,c_partidos:vC_partidos,motivo:vMotivo,condiciones:vCondiciones};
+    let objeto={campeon:vCampeon,nombre:vNombre,apellido:vApellido,correo:vCorreo,rut:vRut,edad:vEdad,c_partidos:vC_partidos,motivo:vMotivo};
     console.log("Objeto creado linea 64"+objeto); //Para verificar que el objeto esta creado
     registrarPersona(objeto).then(() => {
         alert("Persona registrada");
@@ -102,31 +140,14 @@ const registrar = async() => {              //NO FUNCIONA POR QUE LOS DATOS QUED
 
 
 }
-
-const validaciones_front = () => {
-    var campo = document.getElementById(idCampo).value;
-    campo = campo.trim();
-    if(campo == ""){
-        document.getElementById("validar"+idCampo).style.display = "flex";
-        document.getElementById("validar"+idCampo).style.margin = "0px";
-        document.getElementById("validar"+idCampo).style.padding = "0px";
-        document.getElementById("validar"+idCampo).style.color = "red";
-        
-        document.getElementById(idCampo).style.border = "2px solid red";
-        document.getElementById(idCampo).placeholder = "Escribe tu "+idCampo;
-    }else{
-        document.getElementById(idCampo).style.border = "2px solid green";
-        document.getElementById("validar"+idCampo).style.display = "none";
-        var validado = true;
-    }
-    console.log("Valido");
-    return validado;
-
-}
+//-----------------------  REGISTRO COMPLETAMENTE FUNCIONAL Y VALIDADO -------------------------
 
 
-const contraste = () => {
+
+function contraste() {
+    
     var element = document.body;
-        element.classList.toggle("modoOscuro");
+    element.classList.toggle("modoOscuro");
+    console.log("funciona contraste");
 
 }
